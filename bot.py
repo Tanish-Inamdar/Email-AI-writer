acfdc13538f808bdfdc8c16574071f0e04f68d50
Tanish-Inamdar <tanishdinamdar@gmail.com>
2024-07-02 20:57:36 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+// this is the actual bot
 from write_emails import create_email
 from send_emails import send_email
 
@@ -5,4 +6,4 @@ MessageF = create_email()
 subject= input("Subject of the Email: ")
 to = input("To whom shall this fine email send to: ")
 
-send_email(to = to, subject= subject, message= MessageF)
+send_email(to = to, subject= subject, message= MessageF) 
