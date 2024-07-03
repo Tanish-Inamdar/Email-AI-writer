e2ac21c6c5f6a5505f0a18989be6a484d2e7a280
Tanish-Inamdar <tanishdinamdar@gmail.com>
2024-07-02 21:12:55 -0700

    Update read_emails.py

--- read_emails.py
+++ read_emails.py
@@ -2,12 +2,12 @@ from simplegmail import Gmail
 from simplegmail.query import construct_query
 gmail = Gmail()
 query_params = {
-    "newer_than": (20, "day"),
+    "newer_than": (20, "day"), 
     "older_than": (15, "day"),
 
-}
+} 
 
-messages = gmail.get_messages(query=construct_query(query_params))
+messages = gmail.get_messages(query=construct_query(query_params)) //accesses Gmail to get messages and parameters of the email 
 with open("email_samples.txt", "a ") as f:
     for message in messages:
         print("To: " + message.recipient)
@@ -16,6 +16,6 @@ with open("email_samples.txt", "a ") as f:
         print("Date: " + message.date)
         print("Preview: " + message.snippet)
         
-        if message.plain:
+        if message.plain: //makes sure to not include messages above 1000 chracters
             if len(message.plain) < 1000:
                 f.write(message.plain + "\n")
